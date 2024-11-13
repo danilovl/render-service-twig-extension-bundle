5423fb08862e344d63ed9158b74cde1e7b75391d
Vladimir Danilov <dev.danilovl@gmail.com>
2024-11-13 21:06:40 +0100

    RenderServiceExtension - change Extension class.

--- src/DependencyInjection/RenderServiceExtension.php
+++ src/DependencyInjection/RenderServiceExtension.php
@@ -3,9 +3,9 @@
 namespace Danilovl\RenderServiceTwigExtensionBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class RenderServiceExtension extends Extension
 {
